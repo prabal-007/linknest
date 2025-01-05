7ede6b70642a1f6d583727601a8cdac2c7b9e827
Starkk <42775517+prabal-007@users.noreply.github.com>
2025-01-05 23:05:47 +0530

    Add responsiveness

--- linknest/app/page.js
+++ linknest/app/page.js
@@ -12,12 +12,12 @@ export default function Home() {
   return (
     <main>
       <section className="bg-lime-800 min-h-screen grid md:grid-cols-2 items-center">
-        <div className="md:ml-[10vw] text-yellow-400 flex flex-col gap-1 md:px-1 px-4 ">
+        <div className="md:ml-[10vw] text-yellow-400 flex flex-col gap-1 md:px-1 px-4">
           <p className="font-extrabold text-5xl">Nest</p>
           <p className="font-extrabold text-5xl">all your links,</p>
           <p className="font-extrabold text-5xl">at one place.</p>
-          {/* <img src="/image.png" alt="linknest" className="w-fit h-fit mt-12 mb-5 rounded-lg block md:hidden" /> */}
-          <img src="/social.gif" alt="linknest" className="w-fit h-fit mt-12 mb-5 rounded-lg block md:hidden" />
+          <img src="/image.png" alt="linknest" className="w-fit h-fit mt-2 -mb-2 rounded-lg block md:hidden" />
+          {/* <img src="/social.gif" alt="linknest" className="w-fit h-fit mt-2 -mb-2 rounded-lg block md:hidden" /> */}
           <p className="text-sm font-semibold my-4 text-wrap">Join a growing community of creators, entrepreneurs, and businesses who use LinkNest to simplify their digital presence. Share everything you create, promote, and sell with just one "link in bio".</p>
           <div className="flex gap-4">
             <input value={text} onChange={(e) => settext(e.target.value)} type="text" className="text-sm p-2 rounded-md focus:border-green-700 text-black" placeholder="linknest/your-url" />
