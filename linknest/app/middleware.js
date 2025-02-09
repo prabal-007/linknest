62de5b2cf0a408c8bfa137bd4bbf285ad83edaef
Starkk <42775517+prabal-007@users.noreply.github.com>
2025-02-09 23:56:01 +0530

    session management updates

--- linknest/app/middleware.js
+++ linknest/app/middleware.js
@@ -1,12 +1,11 @@
-// /app/middleware.js
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
   pages: {
-    signIn: "/signin", // Redirect to this page if not authenticated
+    signIn: "/signin",
   },
 });
 
 export const config = {
-  matcher: ['/create/*', '/dashboard/*'],  // Matches /create and all subpaths
+  matcher: ['/create/*', '/dashboard*'],
 };
