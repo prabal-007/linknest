b3703a15d4aa081752a00ef8aa1fcfe5d2cb6c5f
Starkk <42775517+prabal-007@users.noreply.github.com>
2025-02-15 18:15:34 +0530

    Dashboard layout updated

--- linknest/app/(dashboard)/dashboard/page.jsx
+++ linknest/app/(dashboard)/dashboard/page.jsx
@@ -3,13 +3,15 @@ import { Wip } from '@/components/wip'
 import React from 'react'
 
 const page = () => {
-  return (<div className='grid grid-cols-4 gap-4 p-5'>
-  <DashCards title={"ViewMee.live"} bg={"bg-gradient-to-r from-rose-300 to-rose-500"}/>
-  <DashCards title={"Resume Builder"} bg={"bg-gradient-to-r from-green-300 to-sky-500"}/>
-  <DashCards title={"Website Builder"} bg={"bg-gradient-to-r from-red-200 to-amber-400"}/>
-  <DashCards title={"Portfolio Builder"} bg={"bg-gradient-to-r from-emerald-100 to-blue-400"}/>
-  <DashCards title={"Stark AI"} bg={"bg-gradient-to-r from-violet-400 to-purple-300"}/>
-  </div>
+  return (<>
+    <div className='md:grid md:grid-cols-4 flex flex-col justify-center items-center gap-4 md:p-5'>
+      <DashCards title={"ViewMee.live"} bg={"bg-gradient-to-r from-rose-300 to-rose-500"} />
+      <DashCards title={"Resume Builder"} bg={"bg-gradient-to-r from-green-300 to-sky-500"} />
+      <DashCards title={"Website Builder"} bg={"bg-gradient-to-r from-red-200 to-amber-400"} />
+      <DashCards title={"Portfolio Builder"} bg={"bg-gradient-to-r from-emerald-100 to-blue-400"} />
+      <DashCards title={"Stark AI"} bg={"bg-gradient-to-r from-violet-400 to-purple-300"} />
+    </div>
+  </>
   )
 }
 
--- linknest/app/(dashboard)/layout.js
+++ linknest/app/(dashboard)/layout.js
@@ -9,16 +9,19 @@ import { MdOutlineContactSupport } from "react-icons/md";
 import { AvatarNameComponent } from "@/components/ui/AvatarComponent";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { MdWorkspacePremium } from "react-icons/md";
-
+import { IoClose } from "react-icons/io5";
+import { RiMenu3Fill } from "react-icons/ri";
+import { RiMenu2Fill } from "react-icons/ri";
 
 
 export default function Layout({
   children,
 }) {
   const router = useRouter();
-  const { data : session } = useSession();
+  const { data: session } = useSession();
+  const [sidebarStatus, setSidebarStatus] = useState(false)
 
   useEffect(() => {
     if (!session) {
@@ -26,32 +29,56 @@ export default function Layout({
     }
   }, [session, router]);
 
+  const handleSidebar = () => {
+    setSidebarStatus(!sidebarStatus)
+  }
   return (<>
-    <div className="flex h-screen backdrop-blur-0">
-      <div className="w-[18vw] flex flex-col items-center justify-between p-2 border-r-8 border-slate-500 my-2 min-h-screen">
-        <div className="flex flex-col items-center">
-          <Link href='/' className='m-2 bg-white w-full flex items-center justify-center p-2 rounded-md'>
-            <img src="/logo-rbg.png" alt="viewMee" className='w-32' />
-          </Link>
-          <div>
-            <SidebarItem href={"/dashboard"} title="Dashboard" icon={<IoHome />} />
-            <SidebarItem href={"/create"} title="Create ViewMee.live" icon={<IoCreate />} />
-            <SidebarItem href={`/review`} title="Review" icon={<VscOpenPreview />} />
-            <SidebarItem href={"/chat"} title="Stark AI" icon={<RiChatAiFill />} />
-            <SidebarItem href={"/website-builder"} title="Website Builder" icon={<MdOutlineContactSupport />} />
+    <div className="flex h-screen w-screen backdrop-blur-0">
+      <div className="">
+        <button onClick={handleSidebar} data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className={`inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg ${sidebarStatus ? 'sm:block' : 'sm:hidden'}  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}>
+          <span className="sr-only">Open sidebar</span>
+          <RiMenu3Fill />
+        </button>
+
+        <aside id="logo-sidebar" className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${sidebarStatus ? 'translate-x-0' : '-translate-x-full'} sm:translate-x-0`} aria-label="page">
+
+          <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-[#1e2640] flex flex-col items-center justify-between p-2">
+            <div className="">
+              <button onClick={handleSidebar} data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className={`sm:hidden  inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg ${sidebarStatus ? 'sm:block' : 'sm:hidden'}  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}>
+                <RiMenu2Fill />
+              </button>
+            {/* </div>
+
+            <div> */}
+              <div className="flex flex-col items-center">
+                <Link href={'/'} className='m-2 bg-white w-full flex items-center justify-center p-2 rounded-md'>
+                  <img src="/logo-rbg.png" alt="viewMee" className='w-32' />
+                </Link>
+                <div>
+                  <SidebarItem href={"/dashboard"} title="Dashboard" icon={<IoHome />} />
+                  <SidebarItem href={"/create"} title="Create ViewMee.live" icon={<IoCreate />} />
+                  <SidebarItem href={`/review`} title="Review" icon={<VscOpenPreview />} />
+                  <SidebarItem href={"/chat"} title="Stark AI" icon={<RiChatAiFill />} />
+                  <SidebarItem href={"/website-builder"} title="Website Builder" icon={<MdOutlineContactSupport />} />
+                </div>
+              </div>
+            </div>
+
+
+            <div className="text-white flex flex-col justify-center items-center">
+              <button className="border border-yellow-100 hover:bg-yellow-500 active:border-black flex justify-center items-center gap-1 rounded-xl text-lg font-mono font-semibold p-2 shadow-md bg-yellow-400 text-black my-1">Get Premium<MdWorkspacePremium />
+              </button>
+              <AvatarNameComponent />
+            </div>
+
           </div>
-        </div>
-        <div className="text-white flex flex-col justify-center items-center">
-          <button className="border border-yellow-100 hover:bg-yellow-500 active:border-black flex justify-center items-center gap-1 rounded-xl text-lg font-mono font-semibold p-2 shadow-md bg-yellow-400 text-black my-1">Get Premium<MdWorkspacePremium />
-          </button>
-          <AvatarNameComponent />
-        </div>
+        </aside>
       </div>
-      <div className="w-full p-2 rounded-lg">
+      <div className="w-full p-2 rounded-lg sm:ml-64">
         {children}
       </div>
     </div>
-    <div className='text-center pb-1 text-white bg-slate-800 md:mt-2'>© 2025 viewMee.live</div>
+    <div className='text-center pb-1 text-white bg-slate-800 md:mt-0'>© 2025 viewMee.live</div>
   </>
-  );
+  )
 }
